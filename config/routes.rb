c0f2055e22aedf58618d5686759e676c39c17eb3
Michał <ul0253566@edu.uni.lodz.pl>
2023-03-04 13:53:05 +0100

    sign_out fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   resources :tasks
   devise_for :users
+
+  devise_scope :user do  
+     get '/users/sign_out' => 'devise/sessions#destroy'     
+  end
+  
   root 'tasks#index'
 end
