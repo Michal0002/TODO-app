808eeba5a4b681a408144ae6ac3f7626d86f8991
Michał <ul0253566@edu.uni.lodz.pl>
2023-09-21 22:22:38 +0200

    host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.hosts << "todo-app-production-9649.up.railway.app"
+  config.hosts << "todo-app-t01q.onrender.com"
   # Code is not reloaded between requests.
   config.cache_classes = true
 
